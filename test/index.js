1abb0374d30a0adb916590dddc75ff416f331392
Larry Botha <larry@fixate.it>
2016-05-27 09:29:21 +0200

    move App test to its own file

--- test/index.js
+++ test/index.js
@@ -1,15 +1,2 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import test from 'tape';
-import App from '../src/assets/js/components/App.jsx';
+import './App';
 
-test('<App /> contains hello world', assert => {
-  const wrapper = shallow(<App />);
-
-  const actual = wrapper.text();
-  const expected = 'Hello World';
-
-  assert.equal(actual, expected, 'returns hello world');
-
-  assert.end();
-});
